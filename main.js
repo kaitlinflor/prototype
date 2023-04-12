39cd60568b74b966bb2cd91033235e14d0f78677
kaitlinflor <kaitlin.flor@gmail.com>
2023-04-12 14:53:52 -0400

    Index and main files

---
+++ main.js
@@ -0,0 +1,23 @@
+// Server_Name = cpsc484-02.yale.internal
+var socket = new WebSocket("ws://[Server_Name]:8888/frames");
+var host = "cpsc484-01.yale.internal:8888";
+
+$(document).ready(function () {
+    frames.start();
+});
+
+var frames = {
+    socket: null,
+
+    start: function () {
+        var url = "ws://" + host + "/frames";
+        frames.socket = new WebSocket(url);
+        frames.socket.onmessage = function (event) {
+            frames.show(JSON.parse(event.data));
+        }
+    },
+
+    show: function (frame) {
+        console.log(frame);
+    }
+};
