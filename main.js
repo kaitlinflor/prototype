9c4f967e314117f117deee4a85c938ecaa63c4f0
kaitlinflor <kaitlin.flor@gmail.com>
2023-04-12 20:42:35 -0400

    Some stuff for first page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Server_Name = cpsc484-02.yale.internal
-var socket = new WebSocket("ws://[Server_Name]:8888/frames");
+var socket = new WebSocket("ws://cpsc484-01.yale.internal:8888/frames");
+// var socket = new WebSocket("ws://[Server_Name]:8888/frames");
 var host = "cpsc484-01.yale.internal:8888";
 
 $(document).ready(function () {
@@ -21,3 +22,18 @@ var frames = {
         console.log(frame);
     }
 };
+
+
+function setup() {
+    // get the dimensions of the parent HTML element
+    height = document.getElementById('sketch-holder').clientHeight;
+    width = document.getElementById('sketch-holder').clientWidth;
+
+    // create canvas
+    var canvas = createCanvas(width, height);
+
+    // stretch canvas to fit dimensions of parent
+    canvas.parent('sketch-holder');
+    canvas.width = width;
+    canvas.height = height;
+}
